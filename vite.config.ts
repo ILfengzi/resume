27d55a86ab47cbe1afa5801f7f39fae19dbb6fec
ILfengzi <2531174288@qq.com>
2025-03-27 00:01:52 +0800

    chore: 修改打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Description:
+ * @Author:
+ * @Date: 2025-03-26 00:10:01
+ * @LastEditTime: 2025-03-26 23:59:23
+ * @LastEditors: Lin Changkun
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -7,14 +14,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: './',
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
